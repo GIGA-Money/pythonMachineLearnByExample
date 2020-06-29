ac23182768005e39303f4e8e692c2b6a5b099436
Christopher Michael Regan <gigachris1@hotmail.com>
2020-06-28 20:22:12 -0400

    nightly update to branch

--- pytorch/README.md
+++ pytorch/README.md
@@ -1,1 +1,2 @@
-# following YT series by user 'Sendex' here: 
+# following YT series by user 'Sentdex' 
+* here: https://www.youtube.com/playlist?list=PLQVvvaa0QuDdeMyHEYc0gxFpYwHY2Qfdh
--- pytorch/sendex_online_01.py
+++ pytorch/sendex_online_01.py
@@ -1,1 +1,25 @@
 import torch
+
+# %%
+x = torch.tensor([84, 39])
+y = torch.tensor([10, 55])
+
+# %%
+print(x * y)
+
+# %%
+x = torch.zeros([2, 3])
+
+print(x)
+
+# %%
+x.shape
+# %%
+torch.size([2, 3])
+# %%
+y = torch.rand([2.5])
+
+# %%
+print(y)
+# %%
+
